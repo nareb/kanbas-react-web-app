9c371cdcb2681b6a10cdcae8a7cbff675b11700d
shakyab <shakya.b@northeastern.edu>
2023-12-04 15:26:53 -0500

    Modified Kanbas>Courses>client.js to update the URL link

--- src/Kanbas/Courses/client.js
+++ src/Kanbas/Courses/client.js
@@ -1,5 +1,12 @@
 import axios from "axios";
-const URL = "https://kanbas-node-server-app-fa23-93c731197fdb.herokuapp.com/api/courses"
+//const URL = "https://kanbas-node-server-app-fa23-93c731197fdb.herokuapp.com/api/courses"
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = `${API_BASE}/courses`;
+
+console.log("API_BASE:", API_BASE);
+console.log("COURSES_URL:", COURSES_URL);
+console.log("MODULES_URL:", MODULES_URL);
 
 export const fetchCourses = async () => {
   // const promise = axios.get("http://localhost:4000/api/courses");
@@ -7,25 +14,25 @@ export const fetchCourses = async () => {
   //   setCourses(response.data);
   // });
 
-  const response = await axios.get(URL);
+  const response = await axios.get(COURSES_URL);
   return response.data;
 };
 
 export const fetchCourse = async (id) => {
-  const response = await axios.get(`${URL}/${id}`);
+  const response = await axios.get(`${COURSES_URL}/${id}`);
   return response.data;
 };
 
 export const deleteCourse = async (id) => {
   const response = await axios.delete(
-    `${URL}/${id}`
+    `${COURSES_URL}/${id}`
   );
   return response.data;
 };
 
 export const updateCourse = async (course) => {
   const response = await axios.put(
-    `${URL}/${course._id}`,
+    `${COURSES_URL}/${course._id}`,
     course
   );
   return response.data;
@@ -33,7 +40,7 @@ export const updateCourse = async (course) => {
 
 export const addCourse = async (course) => {
   const response = await axios.post(
-    URL,
+    COURSES_URL,
     course
   );
   return response.data;
