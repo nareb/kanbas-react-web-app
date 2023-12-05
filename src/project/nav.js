f18c9c6ed482de79a234ecf0ea36134cc04fad23
shakyab <shakya.b@northeastern.edu>
2023-12-04 21:38:45 -0500

    Changed project>nav.js

--- src/project/nav.js
+++ src/project/nav.js
@@ -1,7 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+//import { useSelector } from "react-redux";
 
+function Nav() {
+  const { pathname } = useLocation();
+  const links = [
+    { to: "/project/home", label: "Home" },
+    { to: "/project/search", label: "Search" },
+    { to: "/project/signin", label: "Signin" },
+    { to: "/project/signup", label: "Signup" },
+    { to: "/project/account", label: "Account" },
+    { to: "/project/admin", label: "Admin" },
+    { to: "/project/courses", label: "Courses" },
+  ];
+  const active = (path) => (pathname.includes(path) ? "active" : "");
+  return (
+    <div className="list-group">
+      {links.map((link) => (
+        <Link
+          key={link.to}
+          to={link.to}
+          className={`list-group-item ${active(link.to)}`}
+        >
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Nav;
+
+/*
 function Navigation() {
   const { currentUser } = useSelector((state) => state.userReducer);
   return (
@@ -33,10 +64,12 @@ function Navigation() {
         </Link>
         {/* <Link to="/project/details" className="list-group-item">
         Details
-      </Link> */}
+      </Link> }
       </div>
     </>
   );
 }
 
 export default Navigation;
+*/
+
