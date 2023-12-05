177f6a64f309ac1a39075db966263a46e290b64d
shakyab <shakya.b@northeastern.edu>
2023-12-04 22:25:52 -0500

    Fixing Project deployment issues

--- src/project/nav.js
+++ src/project/nav.js
@@ -11,8 +11,8 @@ function Nav() {
     { to: "/project/signin", label: "Signin" },
     { to: "/project/signup", label: "Signup" },
     { to: "/project/account", label: "Account" },
-    { to: "/project/admin", label: "Admin" },
-    { to: "/project/courses", label: "Courses" },
+    //{ to: "/project/admin", label: "Admin" },
+    //{ to: "/project/courses", label: "Courses" },
   ];
   const active = (path) => (pathname.includes(path) ? "active" : "");
   return (
--- src/project/users/client.js
+++ src/project/users/client.js
@@ -7,57 +7,39 @@ const request = axios.create({
 const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
 const USERS_API = `${API_BASE}/users`;
 
-export const signup = async (credentials) => {
-    const response = await request.post(
-    `${USERS_API}/signup`, credentials);
-    return response.data;
-};
-
 export const signin = async (credentials) => {
   const response = await request.post(`${USERS_API}/signin`, credentials);
+  return response.data;
+};
+export const account = async () => {
+  const response = await request.post(`${USERS_API}/account`);
+  return response.data;
+};
+export const updateUser = async (user) => {
+  const response = await request.put(`${USERS_API}/${user._id}`, user);
+  return response.data;
+};
+export const findAllUsers = async () => {
+  const response = await request.get(`${USERS_API}`);
+  return response.data;
+};
+export const createUser = async (user) => {
+  const response = await request.post(`${USERS_API}`, user);
+  return response.data;
+};
+export const findUserById = async (id) => {
+  const response = await request.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (user) => {
+  const response = await request.delete(`${USERS_API}/${user._id}`);
+  return response.data;
+};
+export const signup = async (user) => {
+  const response = await request.post(`${USERS_API}/signup`, user);
   return response.data;
 };
 export const signout = async () => {
   const response = await request.post(`${USERS_API}/signout`);
   return response.data;
-};
-
-export const account = async () => {
-  const response = await request.post(`${USERS_API}/account`);
-  return response.data;
-};
-
-export const findAllUsers = async () => {
-  const response = await request.get(USERS_API);
-  return response.data;
-};
-
-export const findUserById = async (id) => {
-  const response = await request.get(`${USERS_API}/${id}`);
-  return response.data;
-};
-
-export const createUser = async (user) => {
-  const response = await request.post(`${USERS_API}`, user);
-  return response.data;
-};
-
-export const updateUser = async (id, user) => {
-  const response = await request.put(`${USERS_API}/${id}`, user);
-  return response.data;
-};
-
-export const deleteUser = async (id) => {
-  const response = await request.delete(`${USERS_API}/${id}`);
-  return response.data;
-};
-
-export const checkUsernameAvailability = async (username) => {
-  try {
-    const response = await request.get(`${USERS_API}/checkUsername/${username}`);
-    return response.data.available;
-  } catch (error) {
-    console.error("Error checking username availability:", error);
-    return null;
-  }
 };
